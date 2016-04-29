c4b6292a7fca3baf28a6ead30b76861e6eeeb84c
Pascal van Kooten <kootenpv@gmail.com>
2016-04-29 21:53:26 +0200

    added strength

--- ppm.py
+++ ppm.py
@@ -4,7 +4,8 @@ import platform
 import subprocess
 from getpass import getpass
 
-import pyscrypt
+import pylibscrypt
+
 
 try:
     input = raw_input
@@ -23,18 +24,16 @@ def write_to_clipboard(output):
     subprocess.Popen(cmd, stdin=subprocess.PIPE).communicate(output.encode('utf-8'))
 
 
-def generate(master_password, keyword, cost=2048, oLen=32):
-    hashed = pyscrypt.hash(
-        password=master_password.encode(),
-        salt=keyword.encode(),
-        N=cost,
-        r=1,
-        p=1,
-        dkLen=32
-    )
-    tmp = codecs.encode(hashed, 'hex').decode('utf-8')[0:oLen]
-    tmp = "!A" + tmp[:-2]
-    return tmp
+def generate(master_password, keyword, cost=2 ** 20, oLen=32):
+    hashed = pylibscrypt.scrypt(password=bytes(master_password, "utf8"),
+                                salt=bytes(keyword, "utf8"),
+                                N=cost,
+                                r=8,
+                                p=1,
+                                olen=oLen)
+    res = codecs.encode(hashed, 'hex').decode('utf-8')[:oLen]
+    res = "!A" + res[:-2]
+    return res
 
 if __name__ == "__main__":
     write_to_clipboard(generate(getpass(), input("Keyword: ")))
